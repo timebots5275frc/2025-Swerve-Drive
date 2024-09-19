a8eb2888908accb0dfdeadc1337054540a4a0f0a
BEESECHURGERISGOD <adrianmoen2@gmail.com>
2024-09-19 12:31:44 -0500

    pheonix 6 and some other changes

---
+++ 2025 Swerve Drive/src/main/java/frc/robot/subsystems/DriveTrain/SwerveDrive.java
@@ -0,0 +1,13 @@
+package frc.robot.subsystems.DriveTrain;
+
+import frc.robot.Constants;
+
+public class SwerveDrive {
+
+    Constants constants = new Constants();
+
+    public void drive(double x1, double y1, double x2){
+        
+    }
+    
+}
